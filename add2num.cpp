91829889fd09b44bfa4557177c3b37db6e7743bb
justauser897 <71141157+justauser897@users.noreply.github.com>
2020-12-09 18:44:50 +0200

    Add files via upload

---
+++ add2num.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+	int x, y;
+	int addition;
+	
+	std::cout << "Enter 2 numbers.";
+	std::cin >> x;
+	std::cin >> y;
+	addition = x+y;
+	std::cout << "The result is " << addition;
+	
+	return 0;
+}
